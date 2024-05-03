30e481342b1c65e439dca6118858986939d8a4db
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-05-02 19:57:20 -0600

    GROUP BY

---
+++ 02-seccion/04-group-by.sql
@@ -0,0 +1,14 @@
+
+-- SELECT count(*), followers FROM users 
+-- WHERE followers = 4 OR followers = 4999 GROUP BY followers;
+
+SELECT 
+	COUNT(*), followers 
+FROM 
+	users 
+WHERE 
+	followers BETWEEN 4500 AND 4999
+GROUP BY followers
+ORDER BY followers desc;
+
+
